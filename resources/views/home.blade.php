44655e47b09244f9fd106b44d1238dd3ae1a540a
Prabina Chetri <prabinachetri01@gmail.com>
2022-12-21 17:47:13 +0800

    Delete unnecessary codes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <title>DigiMedia - Creative SEO HTML5 Template</title>
+    <title>FYP Management System</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -46,28 +46,7 @@ https://templatemo.com/tm-568-digimedia
   </div>
   <!-- ***** Preloader End ***** -->
 
-  <!-- Pre-header Starts -->
-  <div class="pre-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-sm-8 col-7">
-          <ul class="info">
-            <li><a href="#"><i class="fa fa-envelope"></i>digimedia@company.com</a></li>
-            <li><a href="#"><i class="fa fa-phone"></i>010-020-0340</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-4 col-sm-4 col-5">
-          <ul class="social-media">
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-behance"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Pre-header End -->
+  
 
   <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
@@ -75,40 +54,40 @@ https://templatemo.com/tm-568-digimedia
       <div class="row">
         <div class="col-12">
           <nav class="main-nav">
+                    <h6>FYP Management System</h6>
+                    
             <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
+            <!-- <a href="index.html" class="logo">
               <img src="assets/images/logo-v3.png" alt="">
-            </a>
+            </a> -->
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
             <ul class="nav">
-              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+              <!-- <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
               <li class="scroll-to-section"><a href="#about">About</a></li>
               <li class="scroll-to-section"><a href="#services">Services</a></li>
               <li class="scroll-to-section"><a href="#portfolio">Projects</a></li>
               <li class="scroll-to-section"><a href="#blog">Blog</a></li>
               <li class="scroll-to-section"><a href="#contact">Contact</a></li> 
-              <li class="scroll-to-section"><div class="border-first-button"><a href="#contact">Free Quote</a></div></li> 
-             
-              @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                    <x-app-layout>
-                     </x-app-layout>
-                    @else
-                       <li> <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+              <li class="scroll-to-section"><div class="border-first-button"><a href="#contact">Free Quote</a></div></li>  -->
+              <li>
+                  @if (Route::has('login'))
+                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        @auth
+                        <x-app-layout>
+                        </x-app-layout>
+                        @else
+                          <li> <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
 
-                        @if (Route::has('register'))
-                         <li>   <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-          
+                            @if (Route::has('register'))
+                            <li>   <a href="{{ route('register') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+           </li>
             </ul>        
-            <a class='menu-trigger'>
-                <span>Menu</span>
-            </a>
+           
             <!-- ***** Menu End ***** -->
           </nav>
         </div>
@@ -126,13 +105,12 @@ https://templatemo.com/tm-568-digimedia
               <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                 <div class="row">
                   <div class="col-lg-12">
-                    <h6>Digital Media Agency</h6>
-                    <h2>We Boost Your Website Traffic</h2>
-                    <p>This template is brought to you by TemplateMo website. Feel free to use this for a commercial purpose. You are not allowed to redistribute the template ZIP file on any other template website. Thank you.</p>
+                    <h6>FYP Management System</h6>
+                    <h2>makes FYP management easier</h2>
                   </div>
                   <div class="col-lg-12">
                     <div class="border-first-button scroll-to-section">
-                      <a href="#contact">Free Quote</a>
+                      <!-- <a href="#contact">Free Quote</a> -->
                     </div>
                   </div>
                 </div>
@@ -140,7 +118,7 @@ https://templatemo.com/tm-568-digimedia
             </div>
             <div class="col-lg-6">
               <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                <img src="assets/images/slider-dec-v3.png" alt="">
+                <img src="assets/images/fyp-banner3.jpg" alt="">
               </div>
             </div>
           </div>
@@ -149,7 +127,7 @@ https://templatemo.com/tm-568-digimedia
     </div>
   </div>
 
-  <div id="about" class="about section">
+  <!-- <div id="about" class="about section">
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
@@ -684,14 +662,14 @@ https://templatemo.com/tm-568-digimedia
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
   <footer>
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>Copyright © 2022 DigiMedia Co., Ltd. All Rights Reserved. 
-          <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a></p>
+          <p>Copyright © 2022 Prabina Chetri. All Rights Reserved. 
+          <br>Design By: Prabina Chetri</a></p>
         </div>
       </div>
     </div>
